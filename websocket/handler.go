264284ce56997ba6991535605c1ae6b986d21c67
Evgenii Paniot <epaniot@nvidia.com>
2025-12-03 10:23:50 +0000

    ignore CloseNormalClosure

--- websocket/handler.go
+++ websocket/handler.go
@@ -85,8 +85,6 @@ func shouldClose(conn *websocket.Conn, remoteAddr string) bool {
 // Expected close codes (not logged):
 // - 1000 CloseNormalClosure: client/gateway initiated graceful close
 // - 1001 CloseGoingAway: server shutting down or browser navigating away
-// - 1006 CloseAbnormalClosure: connection dropped without close frame
-// - 1011 CloseInternalServerErr: upstream error (e.g., gateway canceled stream)
 func handleReadError(err error) {
 	if websocket.IsUnexpectedCloseError(err,
 		websocket.CloseNormalClosure,
